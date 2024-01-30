440ac50eb3eff3525d93dc2ce7113e400fc3f8c5
Caio Rodrigues <86518848+Caio1002@users.noreply.github.com>
2024-01-30 00:00:11 -0300

    Ok!!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import './Header/Header';
 
 function App() {
   return (
-    <div classNameName="App">
-      <header classNameName="App-header">
-        <img src={logo} classNameName="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          classNameName="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header></Header>
   );
 }
 
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,36 +5,34 @@ import smallLeft from '../assets/icons/small-left.png';
 import search from '../assets/icons/search.png';
 
 const Header = () => {
-    return(
+    return (
         <nav className="header__navigation">
 
             <div className="navigation">
                 <button className="arrow-left">
-                    <img src={smallLeft} alt="seta esquerda">
+                    <img src={smallLeft} alt="seta esquerda" />
                 </button>
                 <button className="arrow-right">
-                    <img src={smallRight} alt="seta direita">
+                    <img src={smallRight} alt="seta direita" />
                 </button>
             </div>
 
             <div className="header__search">
-                <img src={search} alt="">
-                <input id="input" type="text" maxlength="800" placeholder="O que você quer ouvir?">
+                <img src={search} alt="" />
+                <input id="input" type="text" maxLength="800" placeholder="O que você quer ouvir?" />
             </div>
 
             <div className="header__login">
-                <div className="header__login">
-                    <button className="subscribe">
-                        Inscreva-se
-                    </button>
-                    <button className="login">
-                        Entrar
-                    </button>
-                </div>
+                <button className="subscribe">
+                    Inscreva-se
+                </button>
+                <button className="login">
+                    Entrar
+                </button>
             </div>
 
         </nav>
-    )
+    );
 };
 
 export default Header;
